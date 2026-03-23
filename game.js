becbad9598c8d22f933da5254d4291b4a0dcd8fc
x68f24btrp-eng <x68f24btrp@privaterelay.appleid.com>
2026-03-24 00:59:46 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,50 +1,84 @@
+//一局の進行を担当する、ゆくゆくはまったとかも
+
+const gameBoard = document.getElementById('gameBoard');
+export const fightBoard = document.getElementById('fightBoard');
+export const restBoardB = document.getElementById('restB');
+export const restBoardA = document.getElementById('restA');
+
+const hurigoma = Math.random();
+const sente = hurigoma < 0.5 ? 'playerA' : 'playerB';
+const gote = sente === 'playerA' ? 'playerB' : 'playerA';
+
 class Game {
+  constructor() {
+    this.teban = sente;
+    this.counter = 1;
+    this.record = [];
+    this.end = false;
+  }
 
-constructor(){
-this.board=new Board();
-this.engine=new Engine(this.board);
-this.turn="sente";
-this.hands={sente:[],gote:[]};
+  tebanUpdate() {
+    this.teban = this.counter % 2 === 1 ? sente : gote;
+  }
 }
 
-move(sx,sy,tx,ty){
+export const observer = new Game();
 
-const p=this.board.get(sx,sy);
-const target=this.board.get(tx,ty);
+let hand = null; //cssがhandになっているマスのindex
+let move = []; //cssがmoveになっているマスのindex
 
-if(target){
-target.side=this.turn;
-target.promoted=false;
-this.hands[this.turn].push(target);
+let kinghand = false;
+
+import { boardData, pieceData, attackA, attackB, selectPiece, transfer, pile } from './board.js';
+
+gameBoard.addEventListener('click', (event) => {
+  if (observer.end === true) return;
+
+  const cell = event.target;
+
+  if (!cell.dataset.index) return;
+
+  const index = parseInt(cell.dataset.index);
+
+  if (boardData[index].cssClass === 'plane') {
+    cssPlane();
+    if (boardData[index].piece !== null && boardData[index].piece.owner === observer.teban) {
+      const handmove = selectPiece(index, kinghand);
+      hand = handmove[0];
+      move = handmove[1];
+    }
+  } else if (boardData[index].cssClass === 'hand') {
+    cssPlane();
+  } else if (boardData[index].cssClass === 'move') {
+    transfer(index, hand);
+    cssPlane();
+    //王手判定
+    aimK();
+    ++observer.counter;
+    observer.tebanUpdate();
+
+    if (kinghand === true && pile(observer.teban) === false) observer.end = true;
+
+  }
+});
+
+//盤上のcssがplaneじゃないところをplaneにする
+function cssPlane() {
+  if (hand !== null) {
+    boardData[hand].cssClass = 'plane';
+    boardData[hand].cssUpdate();
+  }
+
+  move.forEach((data, _) => {
+    boardData[data].cssClass = 'plane';
+    boardData[data].cssUpdate();
+  });
+  hand = null;
+  move = [];
 }
 
-this.board.move(sx,sy,tx,ty);
-
-if(this.canPromote(p,sy,ty)){
-if(confirm("成りますか？")){
-p.promoted=true;
-}
-}
-
-this.turn=this.turn==="sente"?"gote":"sente";
-}
-
-drop(type,x,y){
-
-this.board.set(x,y,{type,side:this.turn,promoted:false});
-
-const hand=this.hands[this.turn];
-const index=hand.findIndex(p=>p.type===type);
-hand.splice(index,1);
-
-this.turn=this.turn==="sente"?"gote":"sente";
-}
-
-canPromote(p,sy,ty){
-if(p.type==="k"||p.type==="g") return false;
-
-if(p.side==="sente") return sy<=2 || ty<=2;
-return sy>=6 || ty>=6;
-}
-
+function aimK() {
+  if (observer.teban === 'playerA' && attackA[pieceData[4].location] !== 0) kinghand = true;
+  else if (observer.teban === 'playerB' && attackB[pieceData[35].location] !== 0) kinghand = true;
+  else kinghand = false;
 }
